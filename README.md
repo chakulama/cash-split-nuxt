d33643ba5dc293b2a0562753de041914219ca1bd
binod lama <binodlama@binods-MacBook-Pro.local>
2024-05-01 13:48:18 +0200

    inti project

---
+++ README.md
@@ -0,0 +1,9 @@
+# cash-split
+
+simple app to split the cost in group
+
+# npm
+npm install
+
+# npm
+npm run dev
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-col items-center p-8">
+    <UButton variant="ghost">button testing</UButton>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+</style>

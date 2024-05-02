4a6c4764832edd7b62755cbad7e1a261afc86063
binod lama <binodlama@binods-MacBook-Pro.local>
2024-05-02 23:05:43 +0200

    udpate

--- components/Calculator.vue
+++ components/Calculator.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>Project Title here(optional)</div>
+  <UInput
+    class="mt-4"
+    color="primary"
+    variant="outline"
+    placeholder="Project title here (optional)"
+  ></UInput>
   <div>
     <CalculatorForm></CalculatorForm>
     <div>share + QR here</div>
